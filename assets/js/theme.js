720e80a5d0d1e952e6e0dea8e8fb1794f80b1cde
Irawan Ria <irawanria85@gmail.com>
2026-04-30 12:27:29 +0700

    .

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -134,3 +134,8 @@ $(function() {
     }
   }
 });
+
+// Blank Target External Links
+$(document.links).filter(function() {
+return this.hostname != window.location.hostname;
+}).attr('target', '_blank');
